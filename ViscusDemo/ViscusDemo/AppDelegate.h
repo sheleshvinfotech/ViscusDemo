288a74401e42cec62991b1e9346d1f1ca5a5fdc2
shelesh rawat <shelesh@vinfotech.com>
2012-01-14 16:15:41 +0530

    First Commit of Viscus Demo

---
+++ ViscusDemo/ViscusDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViscusDemo
+//
+//  Created by Sukrat Ray on 14/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
